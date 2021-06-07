29229a97c6eb4708fca4a4cad352fc2ea144432f
alpageush <71350409+alpageush@users.noreply.github.com>
2021-06-07 17:29:35 +0200

    Add files via upload

---
+++ walidacja_danych/src/main/java/FileUtils.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileUtils {
+    public void saveToFile(String text){
+        try {
+            FileWriter fw = new FileWriter(new File("sekretnehasla.csv"), true);
+            fw.write(text + "\n");
+            fw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(FileUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String readFromFile() {
+        String ret = "";
+        
+        try {
+            Scanner nadzieja = new Scanner(new File("sekretnehasla.csv"));
+            while(nadzieja.hasNextLine()) {
+                String siema = nadzieja.nextLine();
+                ret += siema;
+            }
+            nadzieja.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("blad");
+        }
+        
+        return ret;
+    }
+}
